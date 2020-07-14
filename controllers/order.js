44e1eb08d965bf5347e43c7986a5f8537fcc41ee
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-07-13 22:00:37 -0400

    middlewares order and transactions

--- controllers/order.js
+++ controllers/order.js
@@ -17,19 +17,49 @@ exports.orderById = (req, res, next, id) => {
 };
 
 exports.create = (req, res) => {
-
+    req.body.order.user = req.profile;
+    const order = new Order(req.body.order);
+    order.save((error, data) => {
+        if(error) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    });
 };
 
 exports.listOrders = (req, res) => {
-
+    Order.find()
+        .populate("user", "_id name adress")
+        .sort("-created")
+        .exec((err, orders) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(orders);
+        });
 };
 
 exports.getStatusValues = (req, res) => {
-
+    res.json(Order.schema.path("status").enumValues);
 };
 
 exports.updateOrderStatus = (req, res) => {
-
+    Order.update(
+        {_id: req.body.orderId},
+        { $set: { status: req.body.status } },
+        (err, order => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(order);
+        })
+    );
 };
 
 
